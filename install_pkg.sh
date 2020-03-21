704969e8b0717d57948086477d175c7b585b41ee
caesar <x246libra@hotmail.com>
2020-03-21 18:03:04 +0800

    imwheel

--- install_pkg.sh
+++ install_pkg.sh
@@ -30,6 +30,7 @@ fonts-firacode \
 gdebi \
 dconf-editor \
 cpu-checker \
+imwheel \
 
 wget -P ~/Downloads https://d11yldzmag5yn.cloudfront.net/prod/3.5.359539.0224/zoom_amd64.deb
 sudo aptitude install -y libglib2.0-0 libgstreamer-plugins-base1.0-dev libxcb-shape0 libxcb-shm0 libxcb-xfixes0 libxcb-randr0 libxcb-image0 libfontconfig1 libgl1-mesa-glx libxi6 libsm6 libxrender1 libpulse0 libxcomposite1 libxslt1.1 libsqlite3-0 libxcb-keysyms1 libxcb-xtest0
