6161161d38f92f350830b1d53181271dc3cfd888
caesar.minfeng.tsai <x246libra@hotmail.com>
2020-04-08 06:21:06 +0000

    tune Transparent Huge Pages

--- tune_kernel.sh
+++ tune_kernel.sh
@@ -9,6 +9,12 @@ echo 'net.core.somaxconn=1024' | sudo tee -a /etc/sysctl.conf
 # Transparent Huge Pages (THP)
 sudo chmod +x /etc/rc.d/rc.local
 cat << EOF | sudo tee -a /etc/rc.local
+if test -f /sys/kernel/mm/transparent_hugepage/khugepaged/defrag; then
+  echo 0 > /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
+fi
+if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
+  echo never > /sys/kernel/mm/transparent_hugepage/defrag
+fi
 if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
   echo never > /sys/kernel/mm/transparent_hugepage/enabled
 fi
