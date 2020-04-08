a92de01a6acc3f4dd4056fa84971b4f7bded6c0e
caesar.minfeng.tsai <x246libra@hotmail.com>
2020-04-08 06:04:41 +0000

    tune kernel

---
+++ tune_kernel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# overcommit_memory
+echo 'vm.overcommit_memory=1' | sudo tee -a /etc/sysctl.conf
+
+# TCP backlog setting
+echo 'net.core.somaxconn=1024' | sudo tee -a /etc/sysctl.conf
+
+# Transparent Huge Pages (THP)
+sudo chmod +x /etc/rc.d/rc.local
+cat << EOF | sudo tee -a /etc/rc.local
+if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
+  echo never > /sys/kernel/mm/transparent_hugepage/enabled
+fi
+EOF
+
+# reload /etc/sysctl.conf
+# https://www.opencli.com/linux/sysctl-read-modify-kernel-var
+sudo sysctl -p
