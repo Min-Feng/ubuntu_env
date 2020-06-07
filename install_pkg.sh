7093a3553180439e80e0f9a9314342445351468f
caesar <x246libra@hotmail.com>
2020-06-08 00:29:36 +0800

    install qbittorrent

--- install_pkg.sh
+++ install_pkg.sh
@@ -45,7 +45,8 @@ sudo aptitude install -y \
   imwheel \
   vokoscreen \
   kazam \
-  pcscd pcsc-tools
+  pcscd pcsc-tools \
+  qbittorrent
 
 ## pyenv
 sudo aptitude install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
