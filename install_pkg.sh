d7dd2e482ccf6a154f71274c8fc31ad3fbebead1
caesar <x246libra@hotmail.com>
2020-06-10 00:59:18 +0800

    kolourpaint

--- install_pkg.sh
+++ install_pkg.sh
@@ -47,7 +47,8 @@ sudo aptitude install -y \
   vokoscreen \
   kazam \
   pcscd pcsc-tools \
-  qbittorrent
+  qbittorrent \
+  kolourpaint4
 
 ## pyenv
 sudo aptitude install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
